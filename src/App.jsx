563e9a03571a9dcd87fc10e6c7c9fab71914d76e
MasDrick <maksimbelyh05@gmail.com>
2024-12-13 17:38:51 +0500

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SideBar from './components/SideBar';
+import Home from './pages/Home/Home';
 import './scss/app.scss';
 
 const App = () => (
   <div className="App">
     <SideBar />
+    <Home />
   </div>
 );
 
